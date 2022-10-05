c068c12c72623d66aaa3fab150aa22d9d6d69d7d
Tsuchiya-Aoi <aoi0u0t@gmail.com>
2022-10-05 11:58:38 +0000

    [Fix]応用課題2の解答参照して修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user, only: [:edit, :update, :destroy]
 
   def show
     @book = Book.find(params[:id])
-    @user = @book.user
+    # 修正 renderに直接@book.user記述
+    # @user = @book.user
     @comment = BookComment.new
   end
 
@@ -23,14 +25,14 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    # @book = Book.find(params[:id])
     unless @book.user == current_user
      redirect_to books_path
     end
   end
 
   def update
-    @book = Book.find(params[:id])
+    # @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -39,7 +41,7 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    # @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -48,5 +50,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  # 修正 下記で@bookを定義してるので、edit,update,destroyアクション内の@bookを消してもok
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
